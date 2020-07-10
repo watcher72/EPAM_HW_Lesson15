91d2b848af8a9125bcdd80cd9fd6ccbe1cc6e34e
Watcher <scan.over@mail.ru>
2020-07-10 22:18:12 +0300

    Rename tcp_server.py to top_server_simple.py

---
+++ tcp_client_simple.py
@@ -0,0 +1,24 @@
+import random
+import socket
+import time
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('localhost', 9090))
+
+data = [random.uniform(30, 80) for _ in range(10)]
+
+
+def main():
+    for item in data:
+        time.sleep(2)
+        curr_time = time.localtime()
+        curr_time = time.strftime('%Y-%m-%d %H:%M:%S', curr_time)
+        print(f'Send: {curr_time}: {item:.2f}')
+        sock.send(f'{curr_time}: Client 1 - {item:.2f}\n'.encode('utf-8'))
+
+    sock.close()
+
+
+if __name__ == '__main__':
+    main()
